ae5e420b8c14e54db817c4d0737dab0dd4684f34
Frank Puertas <frankgzmc@gmail.com>
2025-06-17 11:08:05 -0500

    Vpc - Subnet and conection

--- main.tf
+++ main.tf
@@ -43,6 +43,45 @@ resource "aws_security_group" "sg_ec2"{
 
 }
 
+###SUBNET OF VPC###
+resource "aws_subnet" "subnet_public" {
+    vpc_id = aws_vpc.vpc_test.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "us-east-1a"
+    map_public_ip_on_launch = true # Automatically assign public IPs to instances in this subnet
+    tags = {
+        Name = "subnet_public"
+    }
+}
+
+###INTERNET GATEWAY###
+resource "aws_internet_gateway" "my_gateway"{
+    vpc_id = aws_vpc.vpc_test.id
+    tags = {
+        Name = "my_internet_gateway"
+    }
+}
+
+###ROUTE TABLE###
+resource "aws_route_table" "table_route_subnet_public"{
+    vpc_id = aws_vpc.vpc_test.id
+    route {
+        cidr_block = "0.0.0.0/0" # Route all traffic to the internet
+        gateway_id = aws_internet_gateway.my_gateway.id
+    }
+    tags = {
+        Name = "route_table_public"
+    }
+}
+
+###ASSOCIATION OF ROUTE TABLE WITH SUBNET###
+resource "aws_route_table_association" "route_table_association_subnet_public"{
+    subnet_id = aws_subnet.subnet_public.id
+    route_table_id = aws_route_table.table_route_subnet_public.id
+}
+
+
+
 resource "aws_instance" "ec2_test" {
     ami = "ami-020cba7c55df1f615" # AMI of Ubuntu
     instance_type = "t2.micro"
